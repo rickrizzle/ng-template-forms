1aa653ea207a41563aefe98802d0c4fb3af18099
rickrizzle <rickh39@gmail.com>
2019-07-19 14:09:05 -0400

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title = "template-forms"; // this can be deleted
+  formUser = {};
+  onSubmit(form) {
+    console.log("form values ", form);
+  }
+}
